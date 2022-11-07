042d019ad344b7eff6e87640632e700e4f03c80f
Slider666 <112625010+Slider666@users.noreply.github.com>
2022-11-07 16:27:23 +0200

    new6

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,21 +1,25 @@
 import throttle from 'lodash.throttle';
 
 const STORAGE_KEY = 'feedback-form-state';
-const form = document.querySelector('.feedback-form');
+const formRef = document.querySelector('.feedback-form');
+const emailRef = document.querySelector('.feedback-form input');
+const messageRef = document.querySelector('.feedback-form textarea');
 
 const formData = {};
 
-form.addEventListener('submit', onFormSubmit);
+formRef.addEventListener('submit', onFormSubmit);
 
 function onFormSubmit(event) {
-  console.log(JSON.parse(localStorage.getItem(STORAGE_KEY)));
-
+  console.log({
+    email: emailRef.value,
+    message: messageRef.value,
+  });
   event.preventDefault();
   event.target.reset();
   localStorage.removeItem(STORAGE_KEY);
 }
 
-form.addEventListener('input', throttle(onFormData, 500));
+formRef.addEventListener('input', throttle(onFormData, 500));
 
 function onFormData(event) {
   formData[event.target.name] = event.target.value;
@@ -24,29 +28,11 @@ function onFormData(event) {
 
 function populateTextarea() {
   const data = JSON.parse(localStorage.getItem(STORAGE_KEY));
-  const email = document.querySelector('.feedback-form input');
-  const message = document.querySelector('.feedback-form textarea');
+
   if (data) {
-    email.value = data.email;
-    message.value = data.message;
+    emailRef.value = data.email;
+    messageRef.value = data.message;
   }
 }
 
 populateTextarea();
-
-// const textarea = document.querySelector('.feedback-form textarea');
-
-// textarea.addEventListener('input', throttle(onTextareaInput, 500));
-
-// function onTextareaInput(event) {
-//     const message = event.target.value;
-
-//     localStorage.setItem(STORAGE_KEY, message);
-//   }
-
-// function populateTextarea() {
-//   const savedMessage = localStorage.getItem(STORAGE_KEY);
-//   if (savedMessage) {
-//     textarea.value = savedMessage;
-//   }
-// }
