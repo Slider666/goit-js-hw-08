80e83444a8f6642fc4d6179ce7ef60e886300749
Slider666 <112625010+Slider666@users.noreply.github.com>
2022-11-07 16:10:51 +0200

    new4

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -6,24 +6,24 @@ import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const galleryEl = document.querySelector('.gallery');
 
-const newGallary = galleryItems.map(({ preview, original, description }) => {
-  const newEl = document.createElement('div');
-  newEl.classList.add('gallery__item');
-  newEl.insertAdjacentHTML(
-    'beforeend',
-    `<a class="gallery__link" href="${original}">
-        <img
-        class="gallery__image"
-        src="${preview}"
-        data-source="${original}"
-        alt="${description}"
-        />
-    </a>`
-  );
-  return newEl;
-});
+function newGallary(image) {
+  return image
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+              <a class="gallery__link" href="${original}">
+                  <img
+                  class="gallery__image"
+                  src="${preview}"
+                  data-source="${original}"
+                  alt="${description}"
+                  />
+              </a>
+            </div>`;
+    })
+    .join('');
+}
 
-galleryEl.append(...newGallary);
+galleryEl.insertAdjacentHTML('beforeend', newGallary(galleryItems));
 
 new SimpleLightbox('.gallery a', {
   captionsData: 'alt',
